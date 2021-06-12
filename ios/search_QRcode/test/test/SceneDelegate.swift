f12b365d2b76692a58c0eb0d0933c8ca76151cc3
yunshenhsieh <67536792+yunshenhsieh@users.noreply.github.com>
2021-06-12 18:43:08 +0800

    search QR code generator

---
+++ ios/search_QRcode/test/test/SceneDelegate.swift
@@ -0,0 +1,69 @@
+//
+//  SceneDelegate.swift
+//  test
+//
+//  Created by passioner on 2021/5/18.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    // 亮度變數
+    var currentBrightness = 0.0
+/* sceneDidBecomeActive程式碼為『切換到app畫面時，儲存使用者螢幕原始亮度。』 */
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        
+        
+        currentBrightness = Double(UIScreen.main.brightness)
+        /* 亮度提升到最亮
+        UIScreen.main.brightness = 1.0
+        */
+        
+        
+    }
+/* sceneWillResignActive程式碼為『離開app畫面後，把螢幕亮度調回原本使用者的螢幕亮度。 */
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        
+        
+        UIScreen.main.brightness = CGFloat(currentBrightness)
+        
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+        
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+        
+    }
+
+
+}
+
---
+++ ios/search_QRcode/test/test/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by passioner on 2021/5/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var qrImageView: UIImageView!
+    
+    @IBOutlet weak var nameText: UITextField!
+    
+    @IBOutlet weak var displayCode: UILabel!
+    
+    
+    var currentBrightness = 0.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//  下一行程式碼為app開啟時，游標直接在輸入欄位，虛擬鍵盤直接開啟。
+        nameText.becomeFirstResponder()
+        
+    }
+    
+//  下一段函數為碰到畫面任何地方，收起虛擬鍵盤
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        nameText.resignFirstResponder()
+    }
+    
+//  按下『Generate』執行的事情
+    @IBAction func generateAction(_ sender: Any) {
+        let myName = nameText.text
+                if let name = myName{
+                    let combinedString = "\(name)0019999999"
+                    qrImageView.image = generateQRCode(Name:combinedString)
+    }
+    }
+    
+    
+//  QR code產生的程式碼
+    func generateQRCode(Name:String) -> UIImage? {
+        let name_data = Name.data(using: String.Encoding.ascii)
+        if let filter = CIFilter(name: "CIQRCodeGenerator"){
+            filter.setValue(name_data, forKey: "inputMessage")
+            filter.setValue("H", forKey: "inputCorrectionLevel")
+            /* 第一種解法，無高清縮放 開始處*/
+//            let transform = CGAffineTransform(scaleX: 3, y: 3)
+//            if let output = filter.outputImage?.transformed(by: transform){
+//                displayCode.text = nameText.text
+//                nameText.text = ""
+//                return UIImage(ciImage: output)
+//            }
+            /* 第一種解法，無高清縮放 結束處*/
+            
+            if let output = filter.outputImage{
+                let scaleX = qrImageView.frame.size.width / output.extent.size.width
+                let scaleY = qrImageView.frame.size.width / output.extent.size.height
+                let transform = CGAffineTransform(scaleX: scaleX, y: scaleY)
+                let fini = output.transformed(by: transform)
+                
+                // 亮度提升到最高
+                UIScreen.main.brightness = 1.0
+                displayCode.text = nameText.text
+                nameText.text = ""
+                return UIImage(ciImage: fini)
+            }
+        }
+        return nil
+        
+    }
+    
+    }
